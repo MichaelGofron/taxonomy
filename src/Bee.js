d6fc2b844ddd77f8f6d0d2d7ac14b581dfd2eda1
MichaelGofron <gofronm@yahoo.com>
2015-07-08 14:21:37 -0700

    Completed taxonomy basic requirement

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,11 @@
 var Bee = function() {
+  Grub.call(this);
+  //this.prototype = Object.create(Grub);
+  this.age = 5;
+  this.color = "yellow";
+  this.job = "keep on growing";
+
 };
 
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
